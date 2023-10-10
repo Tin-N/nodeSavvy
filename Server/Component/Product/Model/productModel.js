86c4b10566e0061db95b92ac43bc19dada5f8d64
Lucius-Nguyen <nhatccg456@gmail.com>
2023-10-10 16:35:30 +0700

    nhat-commit

--- Server/Component/Product/Model/productModel.js
+++ Server/Component/Product/Model/productModel.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose');
 const Schema= mongoose.Schema;
 const ObjectId=Schema.ObjectId;
 
+
 const productSchema=new Schema({
     id:{type:ObjectId},
     userID:{type:String},
     categoryID:{type:String},
     price:{type:Number},
     detail:{type:String},
-    image:{type:String},
+    image:{type:[String]},
     isApproved:{type:Boolean},
     name:{type:String},
-    quantity:{type:Number}
+    quantity:{type:Number},
+    options:[optionSchema]
+});
+const optionSchema=new Schema({
+   
+    title:{type:String},
+    color:{type:String},
+    size:{type:String},
+    weight:{type:String},
+    imageOption:{type:String}
 });
 module.exports = mongoose.models.product || mongoose.model('product',productSchema);
