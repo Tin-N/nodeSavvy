3c57d09a041a3d36fd0f317cf78c16d6f1fa3378
Lucius-Nguyen <nhatccg456@gmail.com>
2023-11-29 15:04:49 +0700

    nhat-commit

--- Server/routes/Api/OrderDetail.js
+++ Server/routes/Api/OrderDetail.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const orderDetailModel = require('../../Component/order/orderDetailsModel');
+const orderDetailModel = require('../../Component/Order/orderDetailsModel');
 
 
 router.post('/add', async (req, res) => {
