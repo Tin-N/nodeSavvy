8fd4eb57dc63f7ed12897f2d00b7cfda665b66af
n3ko <96611337+n3kofile@users.noreply.github.com>
2023-12-19 00:09:35 +0700

    fix mongodb connect

--- Server/app.js
+++ Server/app.js
@@ -65,10 +65,7 @@ const categoryCpanelRouter= require('./routes/cpanel/CategoryCpanel')
 require('./Component/SaleOff/SaleOffModel')
 const saleOffRouter = require('./routes/Api/saleOffAPI');
 
-mongoose.connect('mongodb://127.0.0.1:27017/SavvyDatabase?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('Database Connected!')).catch(err => console.log('Database Error: ', err));
+mongoose.connect('mongodb+srv://nhatccg456:mcmytFu5xkrw9CwD@cluster0.fn2hqad.mongodb.net/').then(() => console.log('Database Connected!')).catch(err => console.log('Database Error: ', err));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
